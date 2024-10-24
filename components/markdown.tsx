c58261a1d8fceb1851a1d27b8197161399f235ec
Aran Bruce-Caddick <aranbruce@gmail.com>
2024-10-25 00:06:03 +0100

    disabled no children prop rule

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-children-prop */
 
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
